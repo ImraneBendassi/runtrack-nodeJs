27dd684b7d98315378cc3b04b330deef113bc4dd
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-05-13 19:14:13 +0200

    Update index.js

--- jour01/job03/index.js
+++ jour01/job03/index.js
@@ -1,20 +1,3 @@
-const fs = require('fs');
+const count = 3;
 
-
-const filePath = 'C:\\Users\\b13im\\Downloads\\data.txt';
-
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error("Erreur lors de la lecture du fichier:", err);
-    return;
-  }
-
-  
-  let letters = '';
-  for (let i = 0; i < data.length; i += 2) {
-    letters += data[i];
-  }
-  console.log("Lettres sur deux du contenu du fichier:");
-  console.log(letters);
-});
+console.log("La valeur de count est :", count);
