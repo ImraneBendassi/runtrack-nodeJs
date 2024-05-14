767169788b2707591ee0b4bb665f9490c82d2b37
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-05-14 08:48:21 +0200

    Update index.js

--- jour01/job08/index.js
+++ jour01/job08/index.js
@@ -1,20 +1,16 @@
 const fs = require('fs');
 
-
+// Chemin vers le fichier data.txt
 const filePath = 'C:\\Users\\b13im\\Downloads\\data.txt';
 
+// Nouveau contenu à écrire dans le fichier
+const newContent = "Je manipule les fichiers avec un module node !";
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+// Écriture du nouveau contenu dans le fichier
+fs.writeFile(filePath, newContent, (err) => {
   if (err) {
-    console.error("Erreur lors de la lecture du fichier:", err);
+    console.error("Erreur lors de la modification du fichier:", err);
     return;
   }
-
-  
-  let letters = '';
-  for (let i = 0; i < data.length; i += 2) {
-    letters += data[i];
-  }
-  console.log("Lettres sur deux du contenu du fichier:");
-  console.log(letters);
+  console.log("Le contenu du fichier a été modifié avec succès !");
 });
