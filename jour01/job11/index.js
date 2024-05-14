694cf13b3738f94a21b904189731be852263830b
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-05-14 08:57:56 +0200

    Update index.js

--- jour01/job11/index.js
+++ jour01/job11/index.js
@@ -1,20 +1,19 @@
-const fs = require('fs');
+const http = require('http');
 
+// Définition de la fonction de gestion de requête
+const requestHandler = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  response.end('Hello World !\n');
+};
 
-const filePath = 'C:\\Users\\b13im\\Downloads\\data.txt';
+// Création du serveur HTTP
+const server = http.createServer(requestHandler);
 
-
-fs.readFile(filePath, 'utf8', (err, data) => {
+// Écoute du serveur sur le port 8888
+server.listen(8888, (err) => {
   if (err) {
-    console.error("Erreur lors de la lecture du fichier:", err);
-    return;
+    return console.error('Erreur lors du démarrage du serveur :', err);
   }
-
-  
-  let letters = '';
-  for (let i = 0; i < data.length; i += 2) {
-    letters += data[i];
-  }
-  console.log("Lettres sur deux du contenu du fichier:");
-  console.log(letters);
+  console.log('Serveur démarré sur le port 8888');
 });
+    
