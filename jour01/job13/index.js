c3803f0f9f080d9428dd5d39037ba2062903e91d
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-05-14 09:39:09 +0200

    Update index.js

--- jour01/job13/index.js
+++ jour01/job13/index.js
@@ -1,20 +1,39 @@
+const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
+// Créer un serveur HTTP
+const server = http.createServer((req, res) => {
+    let filePath;
 
-const filePath = 'C:\\Users\\b13im\\Downloads\\data.txt';
+    // Vérifier l'URL demandée
+    if (req.url === '/' || req.url === '/index.html') {
+        filePath = path.join(__dirname, 'index.html');
+    } else if (req.url === '/about') {
+        filePath = path.join(__dirname, 'about.html');
+    } else {
+        // Si l'URL demandée n'est pas gérée, renvoyer une réponse 404 (Not Found)
+        res.writeHead(404);
+        return res.end('Page non trouvée.');
+    }
 
+    // Lire le contenu du fichier demandé de manière asynchrone
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            // En cas d'erreur, renvoyer une réponse 500 (Internal Server Error)
+            res.writeHead(500);
+            return res.end('Erreur interne du serveur.');
+        }
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error("Erreur lors de la lecture du fichier:", err);
-    return;
-  }
+        // Définir l'en-tête de réponse avec le type de contenu
+        res.writeHead(200, { 'Content-Type': 'text/html' });
 
-  
-  let letters = '';
-  for (let i = 0; i < data.length; i += 2) {
-    letters += data[i];
-  }
-  console.log("Lettres sur deux du contenu du fichier:");
-  console.log(letters);
+        // Envoyer le contenu du fichier demandé
+        res.end(data);
+    });
+});
+
+// Écouter le serveur sur le port 8888
+server.listen(8888, () => {
+    console.log('Serveur démarré sur le port 8889');
 });
