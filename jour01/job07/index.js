1df99849c4a26e8378507a3fea98ca79491f1039
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-05-14 08:46:05 +0200

    Update index.js

--- jour01/job07/index.js
+++ jour01/job07/index.js
@@ -1,20 +1,15 @@
 const fs = require('fs');
 
-
+// Chemin vers le fichier
 const filePath = 'C:\\Users\\b13im\\Downloads\\data.txt';
 
-
+// Lecture du fichier de manière asynchrone
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.error("Erreur lors de la lecture du fichier:", err);
     return;
   }
-
-  
-  let letters = '';
-  for (let i = 0; i < data.length; i += 2) {
-    letters += data[i];
-  }
-  console.log("Lettres sur deux du contenu du fichier:");
-  console.log(letters);
+  // Affichage du contenu du fichier
+  console.log("Contenu du fichier:");
+  console.log(data);
 });
