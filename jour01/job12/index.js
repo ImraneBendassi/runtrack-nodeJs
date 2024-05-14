35a254c2aa3fd6486691d5fc39d88534d63e4f63
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-05-14 09:26:01 +0200

    Update index.js

--- jour01/job12/index.js
+++ jour01/job12/index.js
@@ -1,20 +1,30 @@
+const http = require('http');
 const fs = require('fs');
+const path = require('path');
+
+// Créer un serveur HTTP
+const server = http.createServer((req, res) => {
+    // Récupérer le chemin complet du fichier index.html
+    const filePath = path.join(__dirname, 'index.html');
+
+    // Lire le contenu du fichier index.html de manière asynchrone
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            // En cas d'erreur, renvoyer une réponse 500 (Internal Server Error)
+            res.writeHead(500);
+            return res.end('Erreur interne du serveur.');
+        }
+
+        // Définir l'en-tête de réponse avec le type de contenu
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        // Envoyer le contenu du fichier index.html
+        res.end(data);
+    });
+});
 
 
-const filePath = 'C:\\Users\\b13im\\Downloads\\data.txt';
-
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error("Erreur lors de la lecture du fichier:", err);
-    return;
-  }
-
-  
-  let letters = '';
-  for (let i = 0; i < data.length; i += 2) {
-    letters += data[i];
-  }
-  console.log("Lettres sur deux du contenu du fichier:");
-  console.log(letters);
+   // Écouter le serveur sur le port 8889
+   server.listen(8889, () => {
+    console.log('Serveur démarré sur le port 8889');
 });
